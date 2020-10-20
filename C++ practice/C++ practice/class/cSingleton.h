8735e17cf2a60e4dd31b6ad3455344c205159786
3355moijam <3355moijam@gmail.com>
2020-10-20 09:12:05 +0900

    .

---
+++ C++ practice/C++ practice/class/cSingleton.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+template <typename T>
+class cSingleton
+{
+private:
+	cSingleton() {};
+public:
+	static T& getInstance()
+	{
+		static T instance;
+		return instance;
+	}
+	cSingleton(T const&) = delete;
+	cSingleton(T&&) = delete;
+	cSingleton& operator=(T const&) = delete;
+	cSingleton& operator=(T &&) = delete;
+};
+
+
+class asdf : public cSingleton<asdf>
+{
+public:
+};
+
+
+
